f5c63f1ee390241794b8d580c8a26abc04c623d4
Alice Cai <ahcai@berkeley.edu>
2018-04-12 14:12:27 -0700

    Set up redux, store, and container

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+
+import Store from './src/Store';
 import { Header } from './src/components';
+import { CryptoContainer } from './src/containers';
 
 export default class App extends Component {
-  render() {
-    return (
-      <View>
-        <Header />
-      </View>
-    );
-  }
+	render() {
+		return (
+			<Provider store={Store}>
+    			<View>
+        			<Header />
+        			<CryptoContainer />
+    			</View>
+    		</Provider>
+    	);
+  	}
 }
