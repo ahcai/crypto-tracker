1161724804ddc26a4b77a5cef9bd414a10080a12
Alice Cai <ahcai@berkeley.edu>
2018-04-12 13:51:26 -0700

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+	header: {
+		fontWeight: 'bold',
+		fontSize: 20,
+	},
+	headerContainer: {
+		display: 'flex',
+		marginTop: 55,
+		alignItems: 'center',
+	},
+})
+
+const Header = () => {
+	const { header, headerContainer } = styles;
+
+	return (
+		<View style={headerContainer}>
+			<Text style={header}>
+				Cryptocurrency App
+			</Text>
+		</View>
+	);
+}
+
+export default Header;
